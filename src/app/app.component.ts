09c43c160b3ac9e94aa0fd5c13b9932dce585342
JulianGomezIb <julian.gomez@inglesparatodos.edu.co>
2022-09-29 18:31:06 -0500

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,26 @@ export class AppComponent {
   title = 'h5p-test';
   editor = H5P.H5PEditor;
 
-  async ngOnInit(): Promise<void> {
-    console.log("entro");
-    
-    const H5P = require('@lumieducation/h5p-server');
+  // async editorh5p(): Promise<void> {
+  //   console.log("entro");
 
-    const h5pEditor = H5P.fs(
-      await new H5P.H5PConfig(
-          new H5P.fsImplementations.JsonStorage(
-              path.resolve('examples/config.json') // the path on the local disc
-              // where the configuration file is stored
-          )
-      ).load(),
-      path.resolve('h5p/libraries'), // the path on the local disc where libraries
-      // should be stored
-      path.resolve('h5p/temporary-storage'), // the path on the local disc where
-      // temporary files (uploads) should
-      // be stored
-      path.resolve('h5p/content') // the path on the local disc where content is
-      // stored
-  );
+  //   const H5P = require('@lumieducation/h5p-server');
 
-  }
+  //   const h5pEditor = H5P.fs(
+  //     await new H5P.H5PConfig(
+  //         new H5P.fsImplementations.JsonStorage(
+  //             path.resolve('examples/config.json') // the path on the local disc
+  //             // where the configuration file is stored
+  //         )
+  //     ).load(),
+  //     path.resolve('h5p/libraries'), // the path on the local disc where libraries
+  //     // should be stored
+  //     path.resolve('h5p/temporary-storage'), // the path on the local disc where
+  //     // temporary files (uploads) should
+  //     // be stored
+  //     path.resolve('h5p/content') // the path on the local disc where content is
+  //     // stored
+  // );
+
+  // }
 }
